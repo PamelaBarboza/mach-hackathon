7d18077b59055fa73694374dc58513d5b9f96335
Geraldine Villacura R <geraldinevillacuraruz@gmail.com>
2021-01-07 22:30:22 -0300

    detalles

--- src/components/goals/Goals.jsx
+++ src/components/goals/Goals.jsx
@@ -2,43 +2,44 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../goals/goals.css";
 
+const Goals = () => {
+  const [name, setName] = React.useState("");
 
-    const Goals = () => {
-        const [name, setName] = React.useState('')
+  const saveData = (e) => {
+    e.preventDefault();
 
-        const saveData = (e) => {
-            e.preventDefault()
+    if (!name.trim()) {
+      console.log("Por Favor, ingresar datos requeridos");
+      return;
+    }
+    // Si ambos campos están correctos, procesamos los datos
 
-            if(!name.trim()){
-                console.log('Por Favor, ingresar datos requeridos')
-                return
-            }
-            // Si ambos campos están correctos, procesamos los datos
+    console.log("Procesando datos..." + name + " ");
+    e.target.reset();
+    setName("");
+  };
 
-            console.log('Procesando datos...'  +  name +  ' ')
-            e.target.reset()
-            setName('')
-        }
-
-        return (
-            <div className="container-goals1">
-            <h2 className="name-goals1">Colócale un nombre</h2>
-            <form onSubmit={ saveData }>
-                <input
-                className="form-goals1"
-                type="text"
-                placeholder="Ejemplo: Celular nuevo"
-                onChange={ (e) => setName(e.target.value) }
-                />
-            </form>
-            <div className="button-goals1">
-            <Link
-            to="/amount">
-                <button>Listo</button>{" "}
-                </Link>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="container-goals1">
+      <div>
+        <h2 className="name-goals1">Colócale un nombre</h2>
+        <form onSubmit={saveData}>
+          <input
+            className="form-goals1"
+            type="text"
+            placeholder="Ejemplo: Celular nuevo"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </form>
+      </div>
+      <div className="button-goals1">
+        <Link to="/amount">
+          <button>Listo</button>{" "}
+        </Link>
+        <hr />
+      </div>
+    </div>
+  );
+};
 
 export default Goals;
--- src/components/messageSuccess/MessageSuccess.jsx
+++ src/components/messageSuccess/MessageSuccess.jsx
@@ -1,19 +1,25 @@
 import React from "react";
-import bot1 from "../../img/bot1.png";
+import bot4 from "../../img/bot4.png";
+import coin3 from "../../img/coin3.png";
 import { Link } from "react-router-dom";
 import "../messageSuccess/messageSuccess.css";
+import { Icon } from "@iconify/react";
+import closeIcon from "@iconify-icons/grommet-icons/close";
 
 const MessageSuccess = () => {
   return (
-    <div className="container-succsse">
+    <div className="container-success">
+      <Icon icon={closeIcon} />
       <h1>¡TU MISIÓN FUE CREADA CON ÉXITO!</h1>
+      <p>Agregamos a tus Machpoint</p>
       <div className="machpoint">
-        <p1>Agregamos a tus Machpoint</p1>
+        <img src={coin3} alt="coin-img" />
+        <span>10</span>
       </div>
-      <img src={bot1} alt="img-bot1" />
-      <Link to="/bot2">
-        <div className="container-benefits">
-          <button>Cerrar</button>
+      <img src={bot4} alt="img-bot1" />
+      <Link to="/nextMission">
+        <div className="container-button-success">
+          <button>Siguiente Misión</button>
         </div>
       </Link>
     </div>
